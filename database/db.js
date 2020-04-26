0284e1a43722266d808abea421e728baf154920c
Ignacio <dev.ignacio.h@gmail.com>
2020-04-26 18:53:52 -0400

    db.js ready

---
+++ database/db.js
@@ -0,0 +1,19 @@
+const Sequelize = require('sequelize')
+const db = {}
+const sequelize = Sequelize('node_vue_todo', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql',
+    operatorsAliases: false,
+
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+})
+
+db.sequelize = sequelize
+db.Sequelize = Sequelize
+
+module.exports = db
